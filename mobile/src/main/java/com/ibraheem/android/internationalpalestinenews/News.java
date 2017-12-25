662a71c412acfec507b4d19999541fbc684e2810
Shinichi02 <aarar90@gmail.com>
2017-12-25 16:41:21 +0200

    Update News.java

--- mobile/src/main/java/com/ibraheem/android/internationalpalestinenews/News.java
+++ mobile/src/main/java/com/ibraheem/android/internationalpalestinenews/News.java
@@ -7,12 +7,14 @@ public class News {
     private final String mselezionenome;
     private final String mdatapubblicata;
     private final String mUrl;
+    private final String mAuthorName;
 
-    public News(String Titolo, String selezionenome, String datapubblicata, String url) {
+    public News(String Titolo, String selezionenome, String datapubblicata, String url , String Author) {
         this.mTitolo = Titolo;
         this.mselezionenome = selezionenome;
         this.mdatapubblicata = datapubblicata;
         this.mUrl = url;
+        this.mAuthorName = Author;
     }
 
     public String getTitolo() {
@@ -29,5 +31,9 @@ public class News {
 
     public String getUrl() {
         return mUrl;
+    }
+
+    public String getAuthorName() {
+        return mAuthorName;
     }
 }
